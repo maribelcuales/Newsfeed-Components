40edd57a5c7b360e9333aef1070d0618fc6174a8
maribelcuales <maribelcuales@gmail.com>
2020-04-30 14:16:56 +0800

    Appended menuList and listItem to parent class menu. Added classList to menu.

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -20,12 +20,6 @@ let menuItems = [
   </div>
 
   The function takes an array as its only argument.
-*/
-
-function createMenu(array) {
-  const menu = document.createElement('div');
-  const menuList = document.createElement('ul');
-  const listItem = document.createElement('li');  
 
   // Step 2: Inside this function, iterate over the array creating a list item <li> element for each item in the array. 
   // Add those items to the <ul>
@@ -35,6 +29,25 @@ function createMenu(array) {
   // Step 4: add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on the menu (your div with a 'menu' class).
 
   // Step 5: return the menu component.
+*/
+
+function createMenu(array) {
+  const menu = document.createElement('div');
+  const menuList = document.createElement('ul');
+  const listItem = document.createElement('li'); 
+
+  menu.appendChild(menuList);
+  menuList.appendChild(listItem);
+
+  menu.classList.add('menu');
+
+  listItem.textContent = array;
+
+  
+
+
+
+
   return menu; 
 }
   
