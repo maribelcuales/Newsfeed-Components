1ee9ffee92805d7653f1dfb0ca5266f721eb5d92
maribelcuales <maribelcuales@gmail.com>
2020-05-01 05:10:59 +0800

    Cleaned up code in Menu.js.

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -34,14 +34,10 @@ let menuItems = [
 function createMenu(array) {
   const menu = document.createElement('div');
   const menuList = document.createElement('ul');
-  //const listItem = document.createElement('li'); 
 
   menu.appendChild(menuList);
-  //menuList.appendChild(listItem);
 
   menu.classList.add('menu');
-
-  //listItem.textContent = array;
 
   array.map(item => {
     const listItem = document.createElement('li'); 
@@ -64,10 +60,5 @@ function createMenu(array) {
   // Step 6: add the menu component to the DOM.
 
 const header = document.querySelector('.header'); 
-
-// menuItems.forEach(item => {
-//   console.log('creating menu', item.array);
-//   menuButton.appendChild(createMenu(item));
-// })
 
 header.appendChild(createMenu(menuItems)); 
